44eb197eb43ecfc073ac88338d3fd9546ce6800d
hariom <hk524569@gmail.com>
2025-05-22 22:57:08 +0530

    completed signature rule

--- SolidPrinciples/LSP_rule/Signature_rule/Cpp/RetureTypeRule.cpp
+++ SolidPrinciples/LSP_rule/Signature_rule/Cpp/RetureTypeRule.cpp
@@ -1,5 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-     
+
+// Return Type Rule :
+// Subtype overriden method return type should be either identical
+// or narrower then the parent method's return type.
+// This is also called as return type covariance.
+// C++ enforces this by covariance.
+
+class Animal
+{
+    // some common Animal methods
+};
+class Dog : public Animal
+{
+    // Additional Dog methods specific to Dogs.
+};
+
+class Parent
+{
+public:
+    virtual Animal *getAnimal()
+    {
+        cout << "Parent : Returning Animal instance" << endl;
+        return new Animal();
+    }
+};
+
+class Child : public Parent
+{
+public:
+    //  Can also have return type as Dog
+    Animal *getAnimal()
+    {
+        cout << "Child : Returning Dog instance" << endl;
+        return new Dog();
+    }
+};
+
+class Client
+{
+private:
+    Parent *p;
+
+public:
+    Client(Parent *p)
+    {
+        this->p = p;
+    }
+    void takeAnimal()
+    {
+        p->getAnimal();
+    }
+};
+int main()
+{
+    Parent *parent = new Parent();
+    Child *child = new Child();
+
+    Client *client = new Client(child);
+    // Client *client = new Client(parent);
+    client->takeAnimal();
 }
